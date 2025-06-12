6650ad0d08604a11308bae0ee671b0bd28751d0f
lll-73 <linym733@gmail.com>
2025-06-12 15:10:57 +0800

    finish of proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,55 +4,55 @@ public class ArrayDeque<T> {
     private int first;
     private int last;
     public ArrayDeque(){
-        size=0;
-        first=0;
-        last=0;
+        size = 0;
+        first = 0;
+        last = 0;
         T[] array = (T[]) new Object[8];
     }
 
     public boolean isEmpty(){
-        return size==0;
+        return size == 0;
     }
 
     private void update(){
-        if (array.length==size){
+        if (array.length == size){
             resize(array.length*2);
-        }else if (size>0 && size*4<array.length && array.length>=16){
-            resize((Math.max(array.length/2,16)));
+        } else if (size > 0 && size * 4 < array.length && array.length >= 16){
+            resize((Math.max(array.length / 2,16)));
         }
     }
     //resize the array
     private void resize(int capacity){
         T[] newArray = (T[]) new Object[capacity];
-        if (first+size-1<=array.length) {
+        if (first + size -1 <= array.length) {
             System.arraycopy(array,first,newArray,first,size);
         } else {
             System.arraycopy(array,first,newArray,first,array.length-first);
             System.arraycopy(array,0,newArray,first+array.length-first,size-array.length+first);
         }
-        array=newArray;
+        array = newArray;
     }
 
     public void addFirst(T item){
         update();
-        if (first==0){
-            first=array.length-1;
+        if (first == 0){
+            first = array.length - 1;
         } else {
-            first=first-1;
+            first = first - 1;
         }
-        array[first]=item;
-        size=size+1;
+        array[first] = item;
+        size = size + 1;
     }
 
     public void addLast(T item){
         update();
-        if (last==array.length-1){
-            last=0;
+        if (last == array.length - 1){
+            last = 0;
         } else {
-            last = last+1;
+            last = last + 1;
         }
-        array[last]=item;
-        size = size+1;
+        array[last] = item;
+        size = size + 1;
     }
 
     public int size(){
@@ -60,17 +60,17 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque(){
-        if(size+first-1> array.length) {
-            for (int i = first; i < array.length; i++) {
+        if(size + first - 1> array.length) {
+            for (int i = first;i < array.length; i++) {
                 System.out.print(array[i]);
                 System.out.print(" ");
             }
-            for(int i=0;i<size-array.length+first;i++){
+            for(int i = 0;i < size - array.length + first;i++){
                 System.out.print(array[i]);
                 System.out.print(" ");
             }
         }else{
-            for (int i =first;i<first+size;i++){
+            for (int i = first;i < first + size;i++){
                 System.out.print(array[i]);
                 System.out.print(" ");
             }
@@ -79,21 +79,21 @@ public class ArrayDeque<T> {
     }
     public T removeFirst(){
         T item=array[first];
-        if(first==array.length-1){
-            first=0;
+        if(first == array.length - 1){
+            first = 0;
         }else{
-            first=first-1;
+            first = first - 1;
         }
         size--;
         update();
         return item;
     }
     public T removeLast(){
-        T item=array[last];
-        if(last==0){
-            last= array.length-1;
+        T item = array[last];
+        if(last == 0){
+            last = array.length - 1;
         }else{
-            last=last-1;
+            last = last - 1;
         }
         size--;
         update();
@@ -101,14 +101,14 @@ public class ArrayDeque<T> {
     }
 
     public T get(int index){
-        if(index<0||index>=first+size){
+        if(index < 0 || index >= first + size){
             return null;
         }
-        if (first+size-1<=array.length) {
+        if (first + size - 1<= array.length) {
             int i = index + first;
             return array[i];
         } else {
-            int i = index+first;
+            int i = index + first;
             if(i<array.length){
                 return array[i];
             } else {
